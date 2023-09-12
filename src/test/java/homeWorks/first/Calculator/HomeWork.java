6aa2ce0099971ae5f2f037e09239a1d5c8f7bbc2
shirguin <shirguin@yandex.ru>
2023-09-12 19:19:06 +0300

    home work seminar_1

--- src/test/java/homeWorks/first/Calculator/HomeWork.java
+++ src/test/java/homeWorks/first/Calculator/HomeWork.java
@@ -7,9 +7,14 @@ import static org.assertj.core.api.Assertions.*;
 public class HomeWork {
     public static void main(String[] args) {
         String error1 = "Не правильный результат работы функции";
-        System.out.println(Calculator.calculatingDiscount(1000, 35));
-        System.out.println(Calculator.calculatingDiscount(1000, 0));
-        assert 650.0 == Calculator.calculatingDiscount(1000, 35):error1;
-        assert 1000.0 == Calculator.calculatingDiscount(1000, 0):error1;
+        assert 650 == Calculator.calculatingDiscount(1000, 35):error1;
+        assert 1000 == Calculator.calculatingDiscount(1000, 0):error1;
+
+        System.out.println(Calculator.calculatingDiscount(1000, -10));
+        System.out.println(Calculator.calculatingDiscount(-1000, 10));
     }
 }
+/**
+ * Java, для меня язык мало знакомый, путаюсь пока в среде разработки
+ * Не понял как с помощью assert написать утверждение для Exception.
+ */
